88a6ab971c1ed43a86b9440282eee776d4cbc9dc
bukun <bukun@osgeo.cn>
2017-05-01 21:39:01 +0800

    mark.

--- DevOps/aliyun2_su.py
+++ DevOps/aliyun2_su.py
@@ -9,7 +9,8 @@ env.password = aliyun2_cfg['root_pass']
 
 
 def restart():
-    run('supervisorctl restart drr')
+    run('supervisorctl restart drr1')
+    run('supervisorctl restart drr2')
     run('supervisorctl restart yunsuan1')
     run('supervisorctl restart yunsuan2')
     run('supervisorctl restart gislab')
