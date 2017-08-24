64e1c704452227bff28505cad4d6d1fc462e0aee
bukun <bukun@osgeo.cn>
2017-08-24 10:32:56 +0800

    clean.

--- bin/bk.stats_lines.py
+++ bin/bk.stats_lines.py
@@ -2,34 +2,43 @@
 # -*- coding:utf-8 -*-
 import os
 import sys
-
+import random
 print(sys.version)
 
 
-def run_lines(inpath, tpname):
+def run_lines(inpath, tpnames):
     fo = open('xx_code_stats.txt', 'w')
     # pwd = os.path.join(os.getcwd(), 'cmi')
 
     proj_ws = os.path.abspath(inpath)
     print(proj_ws)
     cnt_num = 0
-    for wroot, wdirs, wfiles in os.walk(proj_ws):
-        for wfile in wfiles:
 
-            if wfile.endswith(tpname):
-                fo.write('\n'  + wfile + '\n')
-                infile = os.path.join(wroot, wfile)
-                cnts = open(infile).readlines()
-                for cnt in cnts:
-                    cnt = cnt.rstrip()
-                    if len(cnt) > 0:
-                        fo.write("{0}: {1}\n".format(str(cnt_num).zfill(5), cnt))
-                        cnt_num += 1
-                # cnt_num  = cnt_num + len(cnts)
+    for tpname in tpnames:
+        the_files = []
+        for wroot, wdirs, wfiles in os.walk(proj_ws):
+            for wfile in wfiles:
+                if wfile.startswith('__'):
+                    continue
+                if wfile.endswith(tpname):
+                    infile = os.path.join(wroot, wfile)
+                    the_files.append(infile)
+
+        random.shuffle(the_files)
+        for in_file in the_files:
+            wfile2 = in_file[len(proj_ws)+1:]
+            fo.write('\n'  + wfile2 + '\n')
+            cnts = open(in_file).readlines()
+            for cnt in cnts:
+                cnt = cnt.rstrip()
+                if len(cnt) > 0:
+                    fo.write("{0}: {1}\n".format(str(cnt_num).zfill(5), cnt))
+                    cnt_num += 1
+            # cnt_num  = cnt_num + len(cnts)
     fo.close()
     print(cnt_num)
 
 if __name__ == '__main__':
     inpath = sys.argv[1]
-    tpname = sys.argv[2]
-    run_lines(inpath, tpname)
+    tpnames = sys.argv[2:]
+    run_lines(inpath, tpnames)
